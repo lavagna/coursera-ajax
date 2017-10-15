ffc3e4f02202a24997c786391d24f2fcee01887c
Angela Petersen <angela.j.yoo@gmail.com>
2017-10-15 09:17:55 -0700

    added coloring

--- site/module6-solution/app.js
+++ site/module6-solution/app.js
@@ -2,44 +2,53 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController); /*function ($scope) {*/
+.controller('LunchCheckController', LunchCheckController); 
 
   LunchCheckController.$inject = ['$scope', '$filter'];
 
   function LunchCheckController($scope, $filter) {
 
+    // initialize vars
     $scope.lunchItems = "";
     $scope.message = "";
     
     $scope.displayMessage = function () {
 
-      if ($scope.lunchItems == "") {
-        $scope.message = "Please enter data first";      
-      } else {
+      // split string by comma
+      var arrayOfLunchItems = $scope.lunchItems.split(',');
 
-        // split string by comma
-        var arrayOfLunchItems = $scope.lunchItems.split(',');
-
-        // num items including empty content
-        var numLunchItems = arrayOfLunchItems.length;      
-
-        // dis-count empty contents
-        for (var i=0; i<arrayOfLunchItems.length; i++) {
-          var lunchItem = arrayOfLunchItems[i].trim();
-          if (!lunchItem || lunchItem.length === 0) {
-            numLunchItems--;
-          }
-        }
-        
-        // store message in global scope
-        if (numLunchItems == 0) {
-          $scope.message = "Please enter data first"; 
-        } else if (numLunchItems > 0 && numLunchItems <=3) {
-          $scope.message = "Enjoy!";
-        } else {
-          $scope.message = "Too much!";
+      // num items including empty content
+      var numLunchItems = arrayOfLunchItems.length;      
+  
+      // dis-count empty contents : does NOT count empty item as an item
+      for (var i=0; i<arrayOfLunchItems.length; i++) {
+        var lunchItem = arrayOfLunchItems[i].trim();
+        if (!lunchItem || lunchItem.length === 0) {
+          numLunchItems--;
         }
       }
+      
+      // store message in global scope
+      if (numLunchItems == 0) {
+        $scope.message = "Please enter data first"; 
+        $scope.messageStyle = {
+          "color" : "red",
+          "border" : "solid 1px red"
+        }
+      } else if (numLunchItems > 0 && numLunchItems <=3) {
+        $scope.message = "Enjoy!";
+        $scope.messageStyle = {
+          "color" : "green",
+          "border" : "solid 1px green"
+        }
+      } else {
+        $scope.message = "Too much!";
+        $scope.messageStyle = {
+          "color" : "green",
+          "border" : "solid 1px green"
+        }
+      }
+
     };
   }
 })();
