cf2f4c339e840705b2f4e94dfe66e08fe6c30d5b
Angela Petersen <angela.j.yoo@gmail.com>
2017-11-05 07:30:08 -0800

    added directive but not working fully

--- site/module8-solution/app.js
+++ site/module8-solution/app.js
@@ -3,40 +3,52 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
+.factory('NarrowItDownFactory', NarrowItDownFactory)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItemsList', FoundItemsDirective);
 
-NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
-  var nid = this;
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundList.html' //,
+    // scope: {
+    //   items: '<'//,
+    //   // onRemove: '&'
+    // },
+    // controller: NarrowItDownDirectiveController,
+    // controllerAs: 'list',
+    // bindToController: true
+  };
+  
+  return ddo;
+}
 
+function NarrowItDownDirectiveController() {
+  var list = this;
 
-
-  nid.getFoundItems = function(searchText) {
+  list.getFoundItemsFromDirective = function(searchText) {
 
     // Initialize variables
-    nid.errorMessage = "";
-    nid.found = [];
-    
+    list.errorMessage = "";
+    list.found = [];
+
     var searchTextLocal = angular.lowercase(searchText);
     console.log("searchTextLoal = " + searchTextLocal);
   
     if (!searchTextLocal) {
       console.log("search text is empty!");
-      nid.errorMessage = "Nothing found!";
+      list.errorMessage = "Nothing found!";
     } else {
     
       var promise = MenuSearchService.getMatchedMenuItems(searchTextLocal);
 
       promise.then(function (response) {
   
-        nid.found = response;
+        list.found = response;
   
-        // console.log("nid.found = " = nid.found);
-
-        if (nid.found.length === 0) {
+        if (list.found.length === 0) {
           console.log("found array is 0!");
-          nid.errorMessage = "Nothing found!";
+          list.errorMessage = "Nothing found!";
         }
       })
       .catch(function (error) {
@@ -44,7 +56,46 @@ function NarrowItDownController(MenuSearchService) {
       })
     } // end else
 
-    // return nid.found;
+  }
+  return list;
+}
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var list = this;
+  list.found = [];
+  
+  list.getFoundItems = function(searchText) {
+
+    // Initialize variables
+    list.errorMessage = "";
+
+    var searchTextLocal = angular.lowercase(searchText);
+    console.log("searchTextLoal = " + searchTextLocal);
+  
+    if (!searchTextLocal) {
+      console.log("search text is empty!");
+      list.errorMessage = "Nothing found!";
+    } else {
+    
+      var promise = MenuSearchService.getMatchedMenuItems(searchTextLocal);
+      promise.then(function (response) {  
+        console.log("Contoller: response = " + response);
+
+        list.found = response;
+  
+        if (list.found.length === 0) {
+          console.log("found array is 0!");
+          list.errorMessage = "Nothing found!";
+        }
+
+        // return list.found;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    } // end else
+
   };
 
   
@@ -52,85 +103,13 @@ function NarrowItDownController(MenuSearchService) {
 
 }; // end controller
 
-  // var promise = MenuSearchService.getMatchedMenuItems(nid.searchText);
-
-  // console.log("SEARCH TEXT = " + nid.searchText);
-
-  // promise.then(function (response) {
-  //   menu.categories = response.data;
-  // })
-  // .catch(function (error) {
-  //   console.log("Something went terribly wrong.");
-  // });
-
-  //TODO MOVE FUNCTION TO INSIDE SERVICE FUNCTION?
-
-  // nid.logItems = function () {
-  //   console.log("SEARCH TEXT 2 = " + nid.searchText);
-    
-  //   // Initialize variables
-  //   nid.errorMessage = "";
-  //   nid.found = [];
-
-  //   var searchTextLocal = angular.lowercase(nid.searchText);
-
-  //   // Get the list of ONLY matched items
-  //   var promise = MenuSearchService.getMatchedMenuItems2(searchTextLocal);
-
-  //   if (!searchTextLocal) {
-  //     console.log("search text is empty!");
-  //     nid.errorMessage = "Nothing found!";
-  //   } else {
-    
-  //     promise.then(function (response) {
-
-  //       // angular.forEach(response.data, function(value, key) {
-  //       //   angular.forEach(value, function(value, key) {
-            
-  //       //     if ((value.description).indexOf(searchTextLocal) != -1) {
-  //       //       nid.found.push(value);
-  //       //     }
-
-  //       //   });
-  //       // });
-
-  //       if (nid.found.length === 0) {
-  //         console.log("found array is 0!");
-  //         nid.errorMessage = "Nothing found!";
-  //       }
-  //       console.log(nid.found);
-
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     })
-  //   }
-  // };
-
-// }
+ 
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
-  // list of matched items
-  // var found = []; 
-
-  // service.getMatchedMenuItems = function(searchTerm) {
-  //   var response = $http({
-  //     method: "GET",
-  //     url: (ApiBasePath + "/menu_items.json")
-  //   });
-  //   return response;
-  // };
-
   service.getMatchedMenuItems = function(searchTerm) {
-
-    // Get all menu items
-    // var response = $http({
-    //   method: "GET",
-    //   url: (ApiBasePath + "/menu_items.json")
-    // });
 
     return $http({
       method: "GET",
@@ -152,53 +131,17 @@ function MenuSearchService($http, ApiBasePath) {
       console.log("INSIDE getMatchedMenuItems, foundItems = " + foundItems);
       // return processed items
       return foundItems;
-  });
+    });
 
-
-    // console.log("INSIDE getMatchedMenuItems, response = " + response.data);
-    // // Loop through and find ones containing the search term
-    // angular.forEach(response.data, function(value, key) {
-    //   angular.forEach(value, function(value, key) {
-        
-    //     if ((value.description).indexOf(searchTerm) != -1) {
-    //       found.push(value);
-    //     }
-    //   });
-    // });
-
-    // console.log("INSIDE getMatchedMenuItems, found = " + found);
-    // // Return the list of matched items
-    // return found;
   };
 
 }
 
-// MenuCategoriesService.$inject = ['$http', 'ApiBasePath'];
-// function MenuCategoriesService($http, ApiBasePath) {
-//   var service = this;
-
-//   service.getMenuCategories = function () {
-//     var response = $http({
-//       method: "GET",
-//       url: (ApiBasePath + "/categories.json")
-//     });
-
-//     return response;
-//   };
-
-
-//   service.getMenuForCategory = function (shortName) {
-//     var response = $http({
-//       method: "GET",
-//       url: (ApiBasePath + "/menu_items.json"),
-//       params: {
-//         category: shortName
-//       }
-//     });
-
-//     return response;
-//   };
-
-// }
+function NarrowItDownFactory() {
+  var factory = function() {
+    return new NarrowItDownController();
+  };
+  return factory;
+}
 
 })();
