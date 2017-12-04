4df99b4c43ee949ceee96245d28d4850952f3880
Angela Petersen <angela.j.yoo@gmail.com>
2017-12-03 23:37:46 -0800

    Updated myInfo to display registered data. Need to add menu info

--- site/module10-solution/src/public/myinfo/myinfo.controller.js
+++ site/module10-solution/src/public/myinfo/myinfo.controller.js
@@ -8,8 +8,20 @@ MyInfoController.$inject = ['SignUpService'];
 function MyInfoController(SignUpService) {
     var $ctrl = this;
 
+    $ctrl.isSignedUp = false;
+    $ctrl.firstName = '';
+    $ctrl.lastName = '';
+    $ctrl.email = '';
+    $ctrl.phone = '';
+    $ctrl.menuShortName = '';
+    // $ctrl.myInformation = {};
+
+    // Get registered data from service
     if (SignUpService.isSignedUp()) {
         $ctrl.isSignedUp = true;
+        $ctrl.myInformation = SignUpService.getMyInfo();
+
+        console.log("myinfo : " + $ctrl.myInformation);
     }
 
 
--- site/module10-solution/src/public/signup/signup.service.js
+++ site/module10-solution/src/public/signup/signup.service.js
@@ -19,6 +19,7 @@ function SignUpService($http, ApiPath) {
       menuShortName: ''
     };
 
+    // Save registration data
     service.saveMyInfo = function (firstname, lastname, email, phone) {
         service.myInformation.firstname = firstname;
         service.myInformation.lastname = lastname;
@@ -26,21 +27,28 @@ function SignUpService($http, ApiPath) {
         service.myInformation.phone = phone;
     }
 
+    // Save short name
     service.saveShortName = function (shortName) {
         service.myInformation.menuShortName = shortName;
         service.signedUp = true;
     }
 
+    // Test short name
     service.testShortName = function (shortName) {
         return $http.get(ApiPath + '/menu_items/' + shortName + '.json').then(function(response) {
             return response.data;
           });
     }
 
+    // Return whether or not user is already registered
     service.isSignedUp = function() {
         return service.signedUp;
     }
 
+    // Return registered data
+    service.getMyInfo = function() {
+        return service.myInformation;
+    }
 }
 
 
